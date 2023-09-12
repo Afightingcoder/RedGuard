fcaa0573207ccfaebffd0a77e15eba266ed14704
李承轩 <1206817338@qq.com>
2023-09-12 19:40:50 +0800

    first commit

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,23 +11,27 @@ Component({
      * 组件的初始数据
      */
     data: {
-        originFiles: [
-          {
-            url: 'https://tdesign.gtimg.com/miniprogram/images/example4.png',
-            name: 'uploaded1.png',
-            type: 'image',
-          },
-          {
-            url: 'https://tdesign.gtimg.com/miniprogram/images/example6.png',
-            name: 'uploaded2.png',
-            type: 'image',
-          },
-          {
-            url: 'https://tdesign.gtimg.com/miniprogram/images/example5.png',
-            name: 'uploaded1.png',
-            type: 'image',
-          },
-        ],
+      imageIconList: [
+        'https://tdesign.gtimg.com/miniprogram/images/icon1.png',
+        'https://tdesign.gtimg.com/miniprogram/images/<AddCircleIcon />',
+      ],
+        // originFiles: [
+        //   {
+        //     url: 'https://tdesign.gtimg.com/miniprogram/images/example4.png',
+        //     name: 'uploaded1.png',
+        //     type: 'image',
+        //   },
+        //   {
+        //     url: 'https://tdesign.gtimg.com/miniprogram/images/example6.png',
+        //     name: 'uploaded2.png',
+        //     type: 'image',
+        //   },
+        //   {
+        //     url: 'https://tdesign.gtimg.com/miniprogram/images/example5.png',
+        //     name: 'uploaded1.png',
+        //     type: 'image',
+        //   },
+        // ],
         gridConfig: {
           column: 4,
           width: 160,
@@ -42,22 +46,40 @@ Component({
      * 组件的方法列表
      */
     methods: {
-        handleSuccess(e) {
-            const { files } = e.detail;
-            this.setData({
-              originFiles: files,
-            });
-          },
-          handleRemove(e) {
-            const { index } = e.detail;
-            const { originFiles } = this.data;
-            originFiles.splice(index, 1);
-            this.setData({
-              originFiles,
-            });
-          },
+        // handleSuccess(e) {
+        //     const { files } = e.detail;
+        //     this.setData({
+        //       originFiles: files,
+        //     });
+        //   },
+        //   handleRemove(e) {
+        //     const { index } = e.detail;
+        //     const { originFiles } = this.data;
+        //     originFiles.splice(index, 1);
+        //     this.setData({
+        //       originFiles,
+        //     });
+        //   },
           handleClick(e) {
             console.log(e.detail.file);
           },
+          uploadImage() {
+            wx.chooseImage({
+              count: 1, // 可以选择的图片数量，根据需要修改
+              sizeType: ['original', 'compressed'], // 图片的尺寸类型
+              sourceType: ['album', 'camera'], // 图片的来源，可以选择相册或相机
+              success: (res) => {
+                // 选择成功后的回调函数
+                const tempFilePaths = res.tempFilePaths;
+                
+                // 在这里可以上传图片或进行其他处理
+                // 例如：将图片显示在页面上
+                this.setData({
+                  originFiles: [{ url: tempFilePaths[0], type: 'image' }],
+                });
+              },
+            });
+          },
+        
     }
 })
