02a91b9f0f83869c44d573c81c17a47897e3e6e8
Afightingcoder <20223803040@m.scun.edu.cn>
2023-09-12 20:02:49 +0800

    个人中心

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,22 +1,56 @@
 // pages/my/my.js
-// const util = require('../../utils/util.js')
-// const ServiceMockData = require('../../mock/service.js')
-// const LoginMockData = require('../../mock/personinfo.js')
+const app = getApp();
 
 Page({
-    data: {
-      logs: [],
-      image: '',
-      isLoad:false,
-      name:'',
-      star:'',
-      city:'',
-      service:[],
-      settingdata:[
-        {name:'联系客服',icon:'service',type:'service'},
-        {name:'设置',icon:'setting',type:'setting'},
-      ]
-    },
-    
-    
-  })
+  data: {
+    motto: '年龄：19  性别：男',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: false, // 如需尝试获取用户信息可改为false
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '',
+    });
+  },
+  onLoad(option) {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true,
+      });
+    }
+    if (option.release && option.release === 'success') {
+      this.showReleaseMsg();
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        });
+      },
+    });
+  },
+  getUserInfo: function() {
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: (res) => {
+        const userInfo = res.userInfo;
+        console.log(userInfo);
+        // 在此处处理获取到的用户信息，如更新页面数据等操作
+        this.setData({
+            hasUserInfo: true,
+            avatarUrl: userInfo.avatarUrl
+          });
+      },
+    })
+}
+  
+});
